312c1481adba525988f52323634ba28f573b1b97
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-30 18:52:09 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,11 @@
 def square_array(array)
+  i = 0
   primary_colors = ["Red", "Yellow", "Blue"]
 array.each do |number|
-  puts "Primary Color #{color} is #{color.length} letters long."
+  array[]
 end
 end
 
-#You will build a method, `square_array`, that squares each element in an array of numbers 
-#and returns a new array of these squared numbers. Use an iterator 
+#You will build a method, `square_array`, that squares each element in an array of numbers
+#and returns a new array of these squared numbers. Use an iterator
 #and implement your own logic, *don't* use any built-in array methods other than `.each` (e.g. `.collect`, `.inject`).
